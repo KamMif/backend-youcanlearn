c8e36acc928739cd9da956f93f86d40bb647d697
k.miftahov <k.miftahov@cian.ru>
2021-01-16 19:27:22 +0500

    add postgres

---
+++ src/database/connection.ts
@@ -0,0 +1,10 @@
+import database from 'pg';
+
+const Pool = database.Pool;
+export const connections = new Pool({
+  user: 'youcanlearn',
+  host: '165.227.170.11',
+  database: 'youcanlearndb',
+  password: '9191420152',
+  port: 5432
+});
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import {previewRoutes} from "./previewer/routes";
+import { connections } from './database/connection';
 
 export const routes = (app: any) => {
     previewRoutes(app);
@@ -9,4 +10,14 @@ export const routes = (app: any) => {
             return res.status(200).send('pong');
         },
     );
+    app.get('/test-bd', async (req: Request, res: Response) => {
+      const { env } = process;
+      try {
+        const { rows } = await connections.query('SELECT * FROM users')
+        return res.status(200).json({env});
+      } catch (error) {
+        return res.status(400).send(`Error: ${error}`)
+      }
+
+    })
 }
